c80fa19dbd65d640c5b8c9e60de46fa8988102b3
Vlad Denisov <vlad.a.denisov@gmail.com>
2020-06-25 02:22:38 +0300

    general: basic server setup & add prettier

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,22 @@
-import fastify from "fastify";
-
-const server = fastify();
-
-server.get("/ping", async (request, reply) => {
-  return "pong\n";
-});
+import fastify from 'fastify'
+import ws from 'ws'
+const server = fastify()
+const wss = new ws.Server({ server: server.server })
+wss.on('connection', function connection(ws) {
+  console.log('new connection')
+  ws.on('message', function incoming(message) {
+    console.log('received: %s', message)
+  })
+  ws.send('something')
+})
+server.get('/ping', async (request, reply) => {
+  return 'pong\n'
+})
 
 server.listen(8080, (err, address) => {
   if (err) {
-    console.error(err);
-    process.exit(1);
+    console.error(err)
+    process.exit(1)
   }
-  console.log(`Server listening at ${address}`);
-});
+  console.log(`Server listening at ${address}`)
+})
