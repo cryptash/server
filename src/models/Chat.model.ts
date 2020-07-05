599fd01605c4818501a1c95df40d238d877f86e2
Vlad Denisov <vlad.a.denisov@gmail.com>
2020-07-05 13:53:05 +0300

    chats: initial work on db chat model

--- src/models/Chat.model.ts
+++ src/models/Chat.model.ts
@@ -1,1 +1,28 @@
-
+import { Model, DataTypes } from 'sequelize'
+import jwt from 'jsonwebtoken'
+import * as config from '../config.json'
+import sequelize from '../lib/db_connect'
+class Chat extends Model {
+  chat_id!: string
+  messages!: string[]
+  users!: Array<{
+    role: string
+    id: string
+    joined: String
+  }>
+  created_at!: String
+  type!: 'PRIVATE' | 'GROUP'
+  picture_url!: string
+}
+Chat.init(
+  {
+    chat_id: DataTypes.TEXT,
+    messages: DataTypes.ARRAY(DataTypes.TEXT),
+    users: DataTypes.TEXT,
+    created_at: DataTypes.TEXT,
+    type: DataTypes.TEXT,
+    picture_url: DataTypes.TEXT
+  },
+  { tableName: 'Chats', sequelize }
+)
+export default Chat
