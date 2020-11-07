19a1b8092420f8435ab1f204376cb0183d25a804
Vlad Denisov <vlad.a.denisov@gmail.com>
2020-11-07 21:31:36 +0300

    chore: remove js duplicates

--- src/api/messages/send.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var SendMessage = function (clients, message) { };
-exports["default"] = SendMessage;
--- src/lib/db_connect.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var sequelize_1 = require("sequelize");
-var config = require("../config.json");
-var conString = "postgres://" + config.db_user + ":" + config.db_pass + "@" + config.db_host + "/" + config.db_name;
-exports["default"] = new sequelize_1.Sequelize(conString);
