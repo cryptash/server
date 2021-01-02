232941d7580aae5f20327256515cded705ff5698
Vlad Denisov <vlad.a.denisov@gmail.com>
2021-01-02 04:49:32 +0300

    fix: more typed getUserInfo response

--- src/api/user/getUserInfo.ts
+++ src/api/user/getUserInfo.ts
@@ -6,6 +6,24 @@ import Chat from '../../models/Chat.model'
 import Message from '../../models/Message.model'
 import {literal, Op} from 'sequelize'
 
+interface ChatResponse {
+  chat_id: string,
+  messageAt: Date,
+  user: {
+    user_id: string
+    pub_key: string
+    picture: string
+    username: string
+  },
+  messages: Array<{
+    content: string,
+    fromMe: boolean,
+    date: Date,
+    read: boolean,
+    message_id: string
+  }>
+}
+
 const getUserInfo = async (req:  FastifyRequest<{
   Body: {
     user_id: string
@@ -61,15 +79,48 @@ const getUserInfo = async (req:  FastifyRequest<{
       })
       return
     }
+    const response: {
+      username: string,
+      user_id: string,
+      pub_key: string,
+      picture: string,
+      chats: Array<ChatResponse>
+    } = {
+      username: user.username,
+      user_id: token.user_id,
+      pub_key: user.pub_key,
+      picture: user.picture_url,
+      chats: []
+    }
+    user.Chats.forEach(chat => {
+      //@ts-ignore
+      const user = chat['Users'][0]
+      const result: ChatResponse = {
+        chat_id: chat.chat_id,
+        messageAt: chat.messageAt,
+        user: {
+          user_id: user.user_id,
+          pub_key: user.pub_key,
+          picture: user.picture_url,
+          username: user.username,
+        },
+        messages: []
+      }
+      // @ts-ignore
+      chat['Messages'].forEach(msg => {
+        result.messages.push({
+          content: msg.content,
+          fromMe: msg.from === token.user_id,
+          date: msg.createdAt,
+          read: msg.read,
+          message_id: msg.message_id,
+        })
+      });
+      response.chats.push(result)
+    })
     res.status(200).send({
       statusCode: 200,
-      response: {
-        username: user.username,
-        user_id: token.user_id,
-        pub_key: user.pub_key,
-        picture_url: user.picture_url,
-        chats: user.Chats
-      }
+      response
     })
     return
   }
